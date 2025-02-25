a2e2308295904f9c77b47c4067f667b34e39e9c5
Elise <merrifieldelise@arizona.edu>
2025-02-24 21:13:31 -0700

    created new project file and compleated step 1 of the prompt

---
+++ cis129_EliseMerrifield_Lab4.py
@@ -0,0 +1,20 @@
+# Author: Elise Merrifield
+# CIS129 lab4
+# Part 2 Pseudocode: Dual Alternative Decisions
+
+# Step 1
+# declare local variables
+monthlySales = 0 # monthly sales amount
+storeAmount = 0 # store bonus amount
+empAmount = 0 # employee bonus amount
+salesIncrease = 0 # percent of sales increase
+prompt = "What were the monthly sales? $" # prompt will be a string literal
+
+# include code to get the monthly sales amount
+# include code to get the Increase in sales
+# include code to calculate the store bonus
+# include code to calculate the employee bonus
+# include code to print out all the results
+
+# step two
+
